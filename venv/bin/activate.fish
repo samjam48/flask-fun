f0323264ba24893dab0097d39cbc29388e9a0a37
Sam Harris <sam@jam-bc.com>
2017-10-21 00:05:44 +0800

    procfile added

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -36,7 +36,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/samjam/Code/flask-demo/venv"
+set -gx VIRTUAL_ENV "/Users/samjam/Code/flask-funfun/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
