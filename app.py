91e0ee5259d919ad67cc38e58c466f9fa64fd5a4
Sam Harris <sam@jam-bc.com>
2017-10-20 14:07:10 +0800

    refactor dictionary assignment for db

--- app.py
+++ app.py
@@ -25,8 +25,10 @@ def login_required(f):
 def home():
     g.db = connect_db()
     cur = g.db.execute('select *  from posts')
-    posts = [dict(title=row[0], description=row[1]) for row in cur.fetchall()]
-    print(posts)
+    posts = []
+    for row in cur.fetchall():
+        posts.append(dict(title=row[0], description=row[1]))
+
     g.db.close()
     return render_template("index.html", posts=posts)
 
